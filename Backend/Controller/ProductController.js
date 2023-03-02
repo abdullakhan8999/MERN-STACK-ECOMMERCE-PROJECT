a31d0622bf35cf58e6ddd6ff04c8a1da4cceadea
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-03-02 15:56:19 +0530

    Api features : filter,search and pagination.

--- Backend/Controller/ProductController.js
+++ Backend/Controller/ProductController.js
@@ -1,6 +1,7 @@
 const Product = require("../Models/Product.Model");
 const ErrorHandler = require("../Utils/ErrorHandler");
 const catchAsyncError = require("../Middleware/catchAsyncError");
+const ApiFeatures = require("../Utils/apiFeatures");
 
 //Create Product --Admin only
 exports.createProduct = catchAsyncError(async (req, res, next) => {
@@ -9,19 +10,6 @@ exports.createProduct = catchAsyncError(async (req, res, next) => {
     success: true,
     product,
   });
-  // try {
-  //   const product = await Product.create(req.body);
-  //   res.status(201).json({
-  //     success: true,
-  //     product,
-  //   });
-  // } catch (err) {
-  //   console.log(`Error: Product creation failed. Reason: ${err.message}`);
-  //   res.status(400).json({
-  //     success: false,
-  //     error: err.message,
-  //   });
-  // }
 });
 
 //Update product by id --Admin only
@@ -39,29 +27,6 @@ exports.updateProduct = catchAsyncError(async (req, res, next) => {
     success: true,
     product,
   });
-
-  // try {
-  //   let product = await Product.findById(req.params.id);
-  //   if (!product) {
-  //     return next(new ErrorHandler("Product not found", 404));
-  //   }
-
-  //   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-  //     new: true,
-  //     runValidators: true,
-  //     useFindAndModify: false,
-  //   });
-  //   res.status(200).json({
-  //     success: true,
-  //     product,
-  //   });
-  // } catch (err) {
-  //   console.log(`Error: Product update failed. Reason: ${err.message}`);
-  //   res.status(400).json({
-  //     success: false,
-  //     error: err.message,
-  //   });
-  // }
 });
 
 //Delete product by id --Admin only
@@ -75,24 +40,6 @@ exports.deleteProduct = catchAsyncError(async (req, res, next) => {
     success: true,
     message: `${product.name} Product deleted successfully.`,
   });
-  // try {
-  //   let product = await Product.findById(req.params.id);
-  //   if (!product) {
-  //     return next(new ErrorHandler("Product not found", 404));
-  //   }
-
-  //   product = await Product.findByIdAndDelete(req.params.id);
-  //   res.status(200).json({
-  //     success: true,
-  //     message: `${product.name} Product deleted successfully.`,
-  //   });
-  // } catch (err) {
-  //   console.log(`Error: Product delete failed. Reason: ${err.message}`);
-  //   res.status(400).json({
-  //     success: false,
-  //     error: err.message,
-  //   });
-  // }
 });
 
 //get Product details
@@ -106,43 +53,21 @@ exports.getProductDetails = catchAsyncError(async (req, res, next) => {
     success: true,
     product,
   });
-  // try {
-  //   let product = await Product.findById(req.params.id);
-  //   if (!product) {
-  //     return next(new ErrorHandler("Product not found", 404));
-  //   }
-
-  //   res.status(200).json({
-  //     success: true,
-  //     product,
-  //   });
-  // } catch (err) {
-  //   console.log(`Error: Product fetch failed. Reason: ${err.message}`);
-  //   res.status(400).json({
-  //     success: false,
-  //     error: err.message,
-  //   });
-  // }
 });
 
 // Get all product
 exports.getAllProducts = catchAsyncError(async (req, res, next) => {
-  const product = await Product.find();
+  // Api take query , request parameters, Pagination
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments()
+  const apiFeatures = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const product = await apiFeatures.query;
   res.status(200).json({
     success: true,
+    productCount,
     product,
   });
-  // try {
-  //   const product = await Product.find();
-  //   res.status(200).json({
-  //     success: true,
-  //     product,
-  //   });
-  // } catch (err) {
-  //   console.log(`Error: Product fetch failed. Reason: ${err.message}`);
-  //   res.status(400).json({
-  //     success: false,
-  //     error: err.message,
-  //   });
-  // }
 });
--- Backend/Utils/apiFeatures.js
+++ Backend/Utils/apiFeatures.js
@@ -13,9 +13,28 @@ class ApiFeatures {
           },
         }
       : {};
-    console.log(keyword);
     this.query = this.query.find(keyword);
     return this;
+  }
+
+  filter() {
+    const queryCopy = { ...this.queryStr };
+    //removing some fields from the query for category
+    const removeFields = ["keyword", "page", "limit"];
+    removeFields.forEach((field) => delete queryCopy[field]);
+
+    //Filter for price and Rating  here adding $ in options
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
+
+  pagination(resultPerPage){
+   const currentPage = this.query.page || 1;
+   const skip = resultPerPage * currentPage -1;
+   this.query = this.query.limit(resultPerPage).skip(skip);
+   return this; 
   }
 }
 
