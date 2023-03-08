0df58d355e03cd504a9dd665c0ab53596921c143
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-03-08 17:19:00 +0530

    Mongoose and JWTToken errors handler

--- Backend/Middleware/error.js
+++ Backend/Middleware/error.js
@@ -9,6 +9,25 @@ module.exports = (err, req, res, next) => {
     const message = `Resource are not found. Invalid: ${err.path}`;
     err = new ErrorHandler(message, 404);
   }
+
+  //Mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate entry ${Object.keys(err.keyValue)}.`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //MongoDB JWTToken error
+  if (err.name === "jsonWebTokenError") {
+    const message = `Invalid Json web token, try again.`;
+    err = new ErrorHandler(message, 400);
+  }
+
+  //MongoDB JWTToken expired error
+  if (err.name === "TokenExpiredError") {
+    const message = `Invalid Json web expired, try again.`;
+    err = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     status: "fail",
     message: err.message,
