4219d552d448e38e1e5e5eac6d76360484b66c95
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-05-08 13:59:44 +0530

    product details2

--- frontend/src/Component/Product/ProductDetails.jsx
+++ frontend/src/Component/Product/ProductDetails.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Carousel from "react-material-ui-carousel";
+import "./ProductDetails.css";
+import { useDispatch, useSelector } from "react-redux";
+import { getProductDetails } from "../../actions/productAction";
+import { useParams } from "react-router-dom";
 
 export default function ProductDetails() {
+  //getting  product detials from backend to store
+  const dispatch = useDispatch();
+
+  // get id parameter from URL
+  const { id } = useParams();
+
+  // fetch data from store
+  const { product, loading, error } = useSelector(
+    (state) => state.productDetails
+  );
+
+  useEffect(() => {
+    dispatch(getProductDetails(id));
+  }, [dispatch, id]);
   return (
     <>
-      <div className="ProductDetails"></div>
+      <div className="ProductDetails">
+        <div className="">
+          <Carousel>
+            {product &&
+              product.images &&
+              product.images.map((image, i) => (
+                <img
+                  src={image.url}
+                  alt={`${i} Slide`}
+                  key={image.url}
+                  className="CarouselImages"
+                />
+              ))}
+          </Carousel>
+        </div>
+
+        <div className="">
+          <div className="detailsBlock-1">
+
+
+            sdfsd
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
