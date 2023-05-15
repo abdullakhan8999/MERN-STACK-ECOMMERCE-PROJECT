de2917981896c1903c6d2899df83a80ae9859163
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-05-15 13:27:57 +0530

    ProductCard update

--- frontend/src/Component/Home/ProductCard/ProductCard.jsx
+++ frontend/src/Component/Home/ProductCard/ProductCard.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import "./ProductCard.css";
-import { Link } from "react-router-dom";
 import ReactStar from "react-rating-stars-component";
+import { useAlert } from "react-alert";
 
-function ProductCard2({ product }) {
+function ProductCard({ product }) {
+  const alert = useAlert();
+  const handleAddToCart = () => {
+    alert.info("Product added to cart.");
+  };
+  const productDetailsPage = () => {
+    try {
+      window.location.href = `/product/${product._id}`;
+    } catch (error) {
+      alert.error("Failed to fetch products. Please try again later.");
+    }
+  };
   const options = {
     edit: false,
     color: "rgba(20,20,20,0.1)",
@@ -15,32 +26,30 @@ function ProductCard2({ product }) {
   const imageUrl =
     product.images && product.images.length > 0 ? product.images[0].url : null;
   return (
-    <Link to={`/product/${product._id}`}>
-      <div class="productCard-Container">
-        {imageUrl && <img src={imageUrl} alt={product.name} />}
-        <div class="overlay">
-          <div class="items"></div>
-          <div class="items head">
-            <p>{product.name}</p>
-            <hr />
+    <div class="productCard-Container">
+      {imageUrl && <img src={imageUrl} alt={product.name} />}
+      <div class="overlay">
+        <div class="items"></div>
+        <div class="items head" onClick={() => productDetailsPage()}>
+          <p>{product.name}</p>
+          <hr />
+        </div>
+        <div class="items price">
+          <div class="item-rating">
+            <ReactStar classNames="StarRating" {...options} />
+            <span className="review-num">{product.numberOfReview} reviews</span>
           </div>
-          <div class="items price">
-            <div class="item-rating">
-              <ReactStar classNames="StarRating" {...options} />
-              <span className="review-num">
-                {product.numberOfReview} reviews
-              </span>
-            </div>
-            <p class="new">{`₹${product.price}`}</p>
-          </div>
-          <div class="items cart">
-            <i class="fa fa-shopping-cart"></i>
-            <button className="active">ADD TO CART</button>
-          </div>
+          <p class="new">{`₹${product.price}`}</p>
+        </div>
+        <div class="items cart">
+          <i class="fa fa-shopping-cart"></i>
+          <button className="active" onClick={() => handleAddToCart()}>
+            ADD TO CART
+          </button>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
-export default ProductCard2;
+export default ProductCard;
